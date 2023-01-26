fba42eaea49ba168d23570f0354b7a7480d5ad27
Артем <artikus.sol@gmail.com>
2023-01-26 13:42:11 +0400

    RFG - рефакторинг обработки проверки товаров по новым данным

--- includes/wp/class-process-products.php
+++ includes/wp/class-process-products.php
@@ -10,10 +10,12 @@ use Ainsys\Connector\Master\Conditions;
 use Ainsys\Connector\Woocommerce\Helper;
 use Ainsys\Connector\Woocommerce\Prepare_Product_Data;
 use Ainsys\Connector\Woocommerce\Prepare_Product_Variation_Data;
+use Ainsys\Connector\Master\Settings\Admin_UI_Entities_Checking;
 
 class Process_Products extends Process implements Hooked {
 
 	protected static string $entity = 'product';
+
 
 	/**
 	 * Initializes WordPress hooks for plugin/components.
@@ -22,14 +24,11 @@ class Process_Products extends Process implements Hooked {
 	 */
 	public function init_hooks() {
 
-		add_filter( 'ainsys_get_entities_list', array( $this, 'add_product_entity_to_list' ), 10, 1 );
+		add_filter( 'ainsys_get_entities_list', [ $this, 'add_product_entity_to_list' ], 10, 1 );
 
 		/**
 		 * Check entity connection for products
 		 */
-		add_filter( 'ainsys_before_check_connection_make_request', function () {
-			return true;
-		} );
 		add_filter( 'ainsys_check_connection_request', [ $this, 'check_product_entity' ], 15, 3 );
 
 		add_action( 'woocommerce_new_product', 'on_product_save', 10, 1 );
@@ -37,6 +36,87 @@ class Process_Products extends Process implements Hooked {
 		add_action( 'deleted_post', [ $this, 'process_delete' ], 10, 2 );
 
 	}
+
+
+	/**
+	 * @param $entities_list
+	 *
+	 * @return mixed
+	 */
+
+	public function add_product_entity_to_list( $entities_list ) {
+
+		$entities_list['product'] = __( 'Product / fields', AINSYS_CONNECTOR_TEXTDOMAIN );
+
+		/*if ( function_exists( 'wc_coupons_enabled' ) ) {
+			if ( wc_coupons_enabled() ) {
+				$entities_list['coupons'] = __( 'Coupons / fields', AINSYS_CONNECTOR_TEXTDOMAIN );
+			}
+		}*/
+
+		return $entities_list;
+
+	}
+
+
+	/**
+	 * Sends new attachment details to AINSYS
+	 *
+	 * @param  int $product_id
+	 *
+	 * @return void
+	 */
+	public function process_create( int $product_id ): void {
+
+		self::$action = 'CREATE';
+
+		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
+			return;
+		}
+
+		$fields = apply_filters(
+			'ainsys_process_create_fields_' . self::$entity,
+			$this->prepare_data( $product_id ),
+			$product_id
+		);
+
+		$this->send_data( $product_id, self::$entity, self::$action, $fields );
+
+	}
+
+
+	/**
+	 * Sends updated post details to AINSYS.
+	 *
+	 * @param       $product_id
+	 * @param       $product
+	 * @param       $update
+	 */
+	public function process_update( $product_id, $product, $update ): void {
+
+		self::$action = 'UPDATE';
+
+		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
+			return;
+		}
+
+		if ( ! $this->is_updated( $product_id, $update ) ) {
+			return;
+		}
+
+		if ( get_post_type( $product_id ) !== self::$entity ) {
+			return;
+		}
+
+		$fields = apply_filters(
+			'ainsys_process_update_fields_' . self::$entity,
+			$this->prepare_data( $product_id ),
+			$product_id
+		);
+
+		$this->send_data( $product_id, self::$entity, self::$action, $fields );
+	}
+
 
 	/**
 	 * Sends delete post details to AINSYS
@@ -64,133 +144,6 @@ class Process_Products extends Process implements Hooked {
 
 	}
 
-	/**
-	 * Sends new attachment details to AINSYS
-	 *
-	 * @param  int $product_id
-	 *
-	 * @return void
-	 */
-	public function process_create( int $product_id): void {
-
-		self::$action = 'CREATE';
-
-		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
-			return;
-		}
-
-		$fields = apply_filters(
-			'ainsys_process_create_fields_' . self::$entity,
-			$this->prepare_data($product_id),
-			$product_id
-		);
-
-		$this->send_data( $product_id, self::$entity, self::$action, $fields );
-
-	}
-
-	/**
-	 * @param $entities_list
-	 *
-	 * @return mixed
-	 */
-
-	public function add_product_entity_to_list($entities_list){
-
-		$entities_list['product'] = __( 'Product / fields', AINSYS_CONNECTOR_TEXTDOMAIN );
-
-		/*if ( function_exists( 'wc_coupons_enabled' ) ) {
-			if ( wc_coupons_enabled() ) {
-				$entities_list['coupons'] = __( 'Coupons / fields', AINSYS_CONNECTOR_TEXTDOMAIN );
-			}
-		}*/
-
-		return $entities_list;
-
-	}
-
-	/**
-	 * @param $result_entity
-	 * @param $entity
-	 * @param $make_request
-	 *
-	 * @return mixed
-	 * Check "product" entity filter callback
-	 */
-	public function check_product_entity( $result_entity, $entity, $make_request ) {
-
-		$result_test   = $this->get_product();
-		$result_entity = Settings::get_option( 'check_connection_entity' );
-		$result_entity = $this->get_result_entity( $result_test, $result_entity, $entity );
-
-		return $result_entity;
-
-	}
-
-	/**
-	 * @param array $result_test
-	 * @param $result_entity
-	 * @param $entity
-	 *
-	 * @return mixed
-	 */
-	protected function get_result_entity( array $result_test, $result_entity, $entity ) {
-
-		if ( ! empty( $result_test['request'] ) ) {
-			$result_request = $result_test['request'];
-		} else {
-			$result_request = 'Error: Data transfer is disabled. Check the Entities export settings tab';
-		}
-
-		if ( ! empty( $result_test['response'] ) ) {
-			$result_response = $result_test['response'];
-		} else {
-			$result_response = __( 'Error: Data transfer is disabled. Check the Entities export settings tab', AINSYS_CONNECTOR_TEXTDOMAIN );
-		}
-
-		$result_entity[ $entity ] = [
-			'request'        => $result_request,
-			'response'       => $result_response,
-			'short_request'  => mb_substr( Logger::convert_response( $result_request ), 0, 40 ) . ' ... ',
-			'full_request'   => Logger::convert_response( $result_request ),
-			'short_response' => mb_substr( Logger::convert_response( $result_response ), 0, 40 ) . ' ... ',
-			'full_response'  => Logger::convert_response( $result_response ),
-			'time'           => current_time( 'mysql' ),
-			'status'         => false === strpos( $result_response, 'Error:' ),
-		];
-
-		Settings::set_option( 'check_connection_entity', $result_entity );
-
-		return $result_entity;
-	}
-
-	/**
-	 * @return array|false
-	 *
-	 * Get product data for AINSYS
-	 *
-	 */
-
-	private function get_product() {
-
-		$args = array(
-			'limit' => 1,
-		);
-
-		$products = wc_get_products( $args );
-
-		if ( ! empty( $products ) ) {
-
-			$product    = end( $products );
-			$product_id = $product->get_id();
-
-			return $this->process_checking( $product_id, $product, true );
-
-		} else {
-			return false;
-		}
-
-	}
 
 	/**
 	 * Sends updated post details to AINSYS.
@@ -219,44 +172,63 @@ class Process_Products extends Process implements Hooked {
 
 		$fields = apply_filters(
 			'ainsys_process_update_fields_' . self::$entity,
-			$this->prepare_data($product_id),
+			$this->prepare_data( $product_id ),
 			$product_id
 		);
 
 		return $this->send_data( $product_id, self::$entity, self::$action, $fields );
 	}
 
+
 	/**
-	 * Sends updated post details to AINSYS.
+	 * @param                              $result_entity
+	 * @param                              $entity
+	 * @param  Admin_UI_Entities_Checking  $entities_checking
 	 *
-	 * @param       $product_id
-	 * @param       $product
-	 * @param       $update
+	 * @return mixed
+	 * Check "product" entity filter callback
 	 */
-	public function process_update( $product_id, $product, $update ): void {
+	public function check_product_entity( $result_entity, $entity, Admin_UI_Entities_Checking $entities_checking ) {
 
-		self::$action = 'UPDATE';
-
-		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
-			return;
+		if ( $entity !== self::$entity ) {
+			return $result_entity;
 		}
 
-		if ( ! $this->is_updated( $product_id, $update ) ) {
-			return;
-		}
+		$entities_checking->make_request = false;
+		$result_test                     = $this->get_product();
+		$result_entity                   = Settings::get_option( 'check_connection_entity' );
 
-		if ( get_post_type($product_id) !== self::$entity ) {
-			return;
-		}
+		return $entities_checking->get_result_entity( $result_test, $result_entity, $entity );
 
-		$fields = apply_filters(
-			'ainsys_process_update_fields_' . self::$entity,
-			$this->prepare_data($product_id),
-			$product_id
-		);
-
-		$this->send_data( $product_id, self::$entity, self::$action, $fields );
 	}
+
+
+	/**
+	 * @return array
+	 *
+	 * Get product data for AINSYS
+	 *
+	 */
+	protected function get_product(): array {
+
+		$products = wc_get_products( [
+			'limit' => 50,
+		] );
+
+		if ( ! empty( $products ) ) {
+			return [
+				'request'  => __( 'Error: There is no data to check.', AINSYS_CONNECTOR_TEXTDOMAIN ),
+				'response' => __( 'Error: There is no data to check.', AINSYS_CONNECTOR_TEXTDOMAIN ),
+			];
+		}
+
+		$product    = end( $products );
+		$product_id = $product->get_id();
+
+		return $this->process_checking( $product_id, $product, true );
+
+	}
+
 
 	/**
 	 * @param $product_id
@@ -264,26 +236,29 @@ class Process_Products extends Process implements Hooked {
 	 * @return array|mixed|void
 	 * Prepare product data, for send to AINSYS
 	 */
-	public function prepare_data($product_id){
+	public function prepare_data( $product_id ) {
 
 		$data = [];
 
-		$product = wc_get_product($product_id);
+		$product = wc_get_product( $product_id );
 
-		if(!$product){
+		if ( ! $product ) {
 			return $data;
 		}
 
-		$prepare = new Prepare_Product_Data($product);
-		$data = $prepare->prepare_data();
+		$prepare = new Prepare_Product_Data( $product );
+		$data    = $prepare->prepare_data();
 
-		if($product->is_type('variable')){
-			$variations = new Prepare_Product_Variation_Data($product);
+		if ( $product->is_type( 'variable' ) ) {
+			$variations         = new Prepare_Product_Variation_Data( $product );
 			$data['variations'] = $variations->prepare_data();
 		}
 
+		//$helper = new Helper();
+		//var_dump($helper->check_image_exist('http://ainsys.loc/wp-content/uploads/2022/12/1200-675-twitter-linkedin-post-30.png'));
+
 		/*$file = plugin_dir_path(__FILE__) . 'test-simple.json';
-		
+
 		if(file_exists($file)){
 			file_put_contents($file, json_encode($data, JSON_FORCE_OBJECT));
 		}*/
