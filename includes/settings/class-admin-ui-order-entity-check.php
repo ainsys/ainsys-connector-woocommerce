d52e59750cd583e32015db513de740a32f461b08
DmitriyT Ainsys <dmitriy.ainsys@gmail.com>
2023-02-09 12:10:53 +0300

    Update Product & Start Write Orders

--- includes/settings/class-admin-ui-order-entity-check.php
+++ includes/settings/class-admin-ui-order-entity-check.php
@@ -4,20 +4,20 @@ namespace Ainsys\Connector\Woocommerce\Settings;
 
 use Ainsys\Connector\Master\Hooked;
 use Ainsys\Connector\Master\Logger;
-use Ainsys\Connector\Master\Settings\Settings;
-use Ainsys\Connector\Woocommerce\WP\Process_Products;
 use Ainsys\Connector\Master\Settings\Admin_UI_Entities_Checking;
+use Ainsys\Connector\Master\Settings\Settings;
+use Ainsys\Connector\Woocommerce\WP\Process_Orders;
 
-class Admin_Ui_Product_Entity_Check implements Hooked {
+class Admin_Ui_Order_Entity_Check implements Hooked {
 
 	protected $process;
 	protected $entities_checking;
 
-	static public $entity = 'product';
+	static public $entity = 'shop_order';
 
 	public function init_hooks() {
 
-		$this->process = new Process_Products();
+		$this->process = new Process_Orders();
 		$this->entities_checking = new Admin_UI_Entities_Checking();
 
 		/**
@@ -26,7 +26,7 @@ class Admin_Ui_Product_Entity_Check implements Hooked {
 		add_filter( 'ainsys_before_check_connection_make_request', function () {
 			return true;
 		} );
-		add_filter( 'ainsys_check_connection_request', [ $this, 'check_product_entity' ], 15, 3 );
+		add_filter( 'ainsys_check_connection_request', [ $this, 'check_order_entity' ], 15, 3 );
 
 	}
 
@@ -38,17 +38,17 @@ class Admin_Ui_Product_Entity_Check implements Hooked {
 	 * @return mixed
 	 * Check "product" entity filter callback
 	 */
-	public function check_product_entity( $result_entity, $entity, Admin_UI_Entities_Checking $entities_checking) {
+	public function check_order_entity( $result_entity, $entity) {
 
 		if ( $entity !== self::$entity ) {
 			return $result_entity;
 		}
 
 		$this->entities_checking->make_request = false;
-		$result_test   = $this->get_product();
+		$result_test   = $this->get_order();
 		$result_entity = Settings::get_option( 'check_connection_entity' );
 
-		return $entities_checking->get_result_entity($result_test, $result_entity, $entity);
+		return $this->get_result_entity( $result_test, $result_entity, $entity );
 
 	}
 
@@ -59,20 +59,20 @@ class Admin_Ui_Product_Entity_Check implements Hooked {
 	 *
 	 */
 
-	private function get_product() {
+	private function get_order() {
 
 		$args = array(
 			'limit' => 1,
 		);
 
-		$products = wc_get_products( $args );
+		$orders = wc_get_orders( $args );
 
-		if ( ! empty( $products ) ) {
+		if ( ! empty( $orders ) ) {
 
-			$product    = end( $products );
-			$product_id = $product->get_id();
+			$order    = end( $orders );
+			$order_id = $order->get_id();
 
-			return $this->process->process_checking( $product_id, $product, true );
+			return $this->process->process_checking( $order_id, $order, true );
 
 		} else {
 			return false;
