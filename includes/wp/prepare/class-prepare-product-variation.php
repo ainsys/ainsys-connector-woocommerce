eda7013d6d07b76943c5f971afe53f585dbedbc1
Артем <artikus.sol@gmail.com>
2023-03-09 14:59:21 +0400

    RFG - рефаакторинг передачи данных о товаре

--- includes/wp/prepare/class-prepare-product-variation.php
+++ includes/wp/prepare/class-prepare-product-variation.php
@@ -1,24 +1,29 @@
 <?php
 
-namespace Ainsys\Connector\Woocommerce;
+namespace Ainsys\Connector\Woocommerce\WP\Prepare;
 
+use Ainsys\Connector\Woocommerce\Helper;
 use WC_Product_Variable;
 
-class Prepare_Product_Variation_Data extends Prepare_Product_Data {
+class Prepare_Product_Variation extends Prepare_Product {
 
-	protected $variation;
+	protected WC_Product_Variable $variation;
 
-	public function __construct( $product ) {
+
+	public function __construct( WC_Product_Variable $product ) {
+
 		parent::__construct( $product );
 
 		$this->variation = new WC_Product_Variable( $this->product->get_id() );
-		
+
 	}
 
+
 	/**
-	 * @return mixed|void
+	 * @return array
 	 */
-	public function prepare_data() {
+	public function prepare_data(): array {
+
 		/**
 		 * Add Extra data to default array
 		 */
@@ -26,11 +31,14 @@ class Prepare_Product_Variation_Data extends Prepare_Product_Data {
 
 		$data = $this->variation->get_available_variations();
 
-		return apply_filters( 'ainsys_prepared_product_variation_data_before_send',
-		                      $data,
-		                      $this->product,
-		                      $this->variation );
+		return apply_filters(
+			'ainsys_prepared_product_variation_data_before_send',
+			$data,
+			$this->product,
+			$this->variation
+		);
 	}
+
 
 	/**
 	 * @param $data
@@ -39,7 +47,8 @@ class Prepare_Product_Variation_Data extends Prepare_Product_Data {
 	 *
 	 * @return array
 	 */
-	public function set_variation_data_list( $data, $obj, $variation ) {
+	public function set_variation_data_list( $data, $obj, $variation ): array {
+
 		$data['ID'] = $variation->get_id();
 
 		$data = $this->unset_data( $data );
@@ -57,7 +66,7 @@ class Prepare_Product_Variation_Data extends Prepare_Product_Data {
 		/**
 		 * Setup Images info
 		 */
-		$data['image'] = parent::get_image_data_by_id( $variation->get_image_id() );
+		$data['image'] = $this->get_image_data_by_id( $variation->get_image_id() );
 
 		/**
 		 * Setup Variation Shipping Info
@@ -70,26 +79,27 @@ class Prepare_Product_Variation_Data extends Prepare_Product_Data {
 		$data = $this->setup_variation_tax_info( $data, $variation );
 
 		$downloadable_info = $this->get_downloadable_info_for_variation( $variation );
-		$data                 = array_merge( $data, $downloadable_info );
+
+		$data = array_merge( $data, $downloadable_info );
+
 		$data['variation_id'] = $variation->get_id();
 
-		$data = $this->setup_variation_attributes($data, $variation);
-
-		return $data;
+		return $this->setup_variation_attributes( $data, $variation );
 	}
 
-	public function setup_variation_attributes($data, $variation){
+
+	public function setup_variation_attributes( $data, $variation ): array {
 
 		$attributes = $variation->get_attributes();
 
-		if(!empty($attributes)){
-			foreach($attributes as $attribute_key => $attribute_value){
+		if ( ! empty( $attributes ) ) {
+			foreach ( $attributes as $attribute_key => $attribute_value ) {
 
-				if(strpos( $attribute_key, 'attribute_' ) === false){
+				if ( strpos( $attribute_key, 'attribute_' ) === false ) {
 					$attribute_key = 'attribute_' . $attribute_key;
 				}
 
-				$data['attributes'][$attribute_key] = Helper::format_term_value(
+				$data['attributes'][ $attribute_key ] = Helper::format_term_value(
 					$attribute_value, $attribute_key,
 					'slug', 'name'
 				);
@@ -99,26 +109,32 @@ class Prepare_Product_Variation_Data extends Prepare_Product_Data {
 		return $data;
 	}
 
+
 	/**
+	 * Setup Variation Shipping info
+	 *
 	 * @param $data
 	 * @param $variation
 	 *
 	 * @return mixed
-	 * Setup Variation Shipping info
+	 *
 	 */
 	public function setup_variation_shipping_info( $data, $variation ) {
+
 		$data['purchase_note']     = $variation->get_purchase_note();
-		$data['weight']            = $variation->get_weight();
-		$data['length']            = $variation->get_length();
-		$data['width']             = $variation->get_width();
-		$data['height']            = $variation->get_height();
+		$data['weight']            = (int) $variation->get_weight();
+		$data['length']            = (int) $variation->get_length();
+		$data['width']             = (int) $variation->get_width();
+		$data['height']            = (int) $variation->get_height();
 		$data['shipping_class_id'] = $variation->get_shipping_class_id();
 		$data['shipping_class']    = $variation->get_shipping_class();
 
 		return $data;
 	}
 
+
 	public function setup_variation_tax_info( $data, $variation ) {
+
 		$data['taxable']    = $variation->is_taxable();
 		$data['tax_status'] = $variation->get_tax_status();
 		$data['tax_class']  = $variation->get_tax_class();
@@ -126,14 +142,18 @@ class Prepare_Product_Variation_Data extends Prepare_Product_Data {
 		return $data;
 	}
 
+
 	/**
+	 * Setup Variation price info
+	 *
 	 * @param $data
 	 * @param $variation
 	 *
 	 * @return mixed
-	 * Setup Variation price info
+	 *
 	 */
 	public function setup_variation_price_info( $data, $variation ) {
+
 		$data['price']             = (int) $variation->get_price();
 		$data['regular_price']     = (int) $variation->get_regular_price();
 		$data['sale_price']        = (int) $variation->get_sale_price();
@@ -143,14 +163,18 @@ class Prepare_Product_Variation_Data extends Prepare_Product_Data {
 		return $data;
 	}
 
+
 	/**
+	 * Setup stock info
+	 *
 	 * @param $data
 	 * @param $variation
 	 *
 	 * @return mixed
-	 * Setup stock info
+	 *
 	 */
 	public function setup_variation_stock_info( $data, $variation ) {
+
 		$data['manage_stock']       = $variation->get_manage_stock();
 		$data['stock_qty']          = $variation->get_stock_quantity();
 		$data['stock_status']       = $variation->get_stock_status();
@@ -161,48 +185,55 @@ class Prepare_Product_Variation_Data extends Prepare_Product_Data {
 		return $data;
 	}
 
+
 	/**
+	 * Unset not needed data from prepare data to AINSYS
+	 *
 	 * @param $data
 	 *
 	 * @return mixed
-	 * Unset not needed data from prepare data to AINSYS
+	 *
 	 */
 	public function unset_data( $data ) {
-		unset( $data['availability_html'] );
-		unset( $data['dimensions_html'] );
-		unset( $data['display_price'] );
-		unset( $data['display_regular_price'] );
-		unset( $data['price_html'] );
-		unset( $data['weight_html'] );
-		unset( $data['is_in_stock'] );
-		unset( $data['backorders_allowed'] );
-		unset( $data['is_sold_individually'] );
-		unset( $data['image'] );
-		unset( $data['max_qty'] );
-		unset( $data['min_qty'] );
-		unset( $data['dimensions'] );
-		unset( $data['is_downloadable'] );
-		unset( $data['variation_is_active'] );
-		unset( $data['variation_is_visible'] );
+
+		unset(
+			$data['availability_html'],
+			$data['dimensions_html'],
+			$data['display_price'],
+			$data['display_regular_price'],
+			$data['price_html'],
+			$data['weight_html'],
+			$data['is_in_stock'],
+			$data['backorders_allowed'],
+			$data['is_sold_individually'],
+			$data['image'], $data['max_qty'],
+			$data['min_qty'],
+			$data['dimensions'],
+			$data['is_downloadable'],
+			$data['variation_is_active'],
+			$data['variation_is_visible']
+		);
 
 		return $data;
 	}
 
+
 	/**
+	 * Setup downloads data for variation
+	 *
 	 * @param $variation
 	 *
 	 * @return array
-	 * Setup downloads data for variation
+	 *
 	 */
-	public function get_downloadable_info_for_variation( $variation ) {
-		$data = [
+	public function get_downloadable_info_for_variation( $variation ): array {
+
+		return [
 			'downloadable'    => $variation->get_downloadable(),
 			'downloads'       => $this->get_product_downloads( $variation ),
 			'download_expiry' => $variation->get_download_expiry(),
 			'download_limit'  => $variation->get_download_limit(),
 		];
-
-		return $data;
 	}
 
 }
