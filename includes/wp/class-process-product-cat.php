673434d0fa2fe2736746da578acfc8edf9fa715d
Артем <artikus.sol@gmail.com>
2023-03-09 23:22:51 +0400

    RFG - рефакторинг, отладка передачи данных o категориях и метках

--- includes/wp/class-process-product-cat.php
+++ includes/wp/class-process-product-cat.php
@@ -4,12 +4,12 @@ namespace Ainsys\Connector\Woocommerce\WP;
 
 use Ainsys\Connector\Master\Conditions;
 use Ainsys\Connector\Master\Hooked;
-use Ainsys\Connector\Master\WP\Process;
-use Ainsys\Connector\Woocommerce\Prepare_Product_Cat_Data;
+use Ainsys\Connector\Master\WP\Prepare\Prepare_Taxonomies;
 
-class Process_Product_Cat extends Process implements Hooked{
+class Process_Product_Cat extends Prepare_Taxonomies implements Hooked {
 
 	protected static string $entity = 'product_cat';
+
 
 	/**
 	 * Initializes WordPress hooks for plugin/components.
@@ -18,77 +18,19 @@ class Process_Product_Cat extends Process implements Hooked{
 	 */
 	public function init_hooks() {
 
-		add_action('created_product_cat', [$this, 'process_create'], 10, 3);
-		add_action('delete_product_cat', [$this, 'process_delete'], 10, 4);
-		add_action('edited_product_cat', [$this, 'process_update'], 10, 3);
+		add_action( 'created_' . self::$entity, [ $this, 'process_create' ], 10, 1 );
+		add_action( 'edited_' . self::$entity, [ $this, 'process_update' ], 10, 1 );
+		add_action( 'delete_' . self::$entity, [ $this, 'process_delete' ], 10, 1 );
 
 	}
 
-	/**
-	 * @param $term_id
-	 * @param $tt_id
-	 * @param $args
-	 *
-	 * Sends updated Product Cat details to AINSYS.
-	 */
-	public function process_update( $term_id, $tt_id, $args ): void {
-
-		self::$action = 'UPDATE';
-
-		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
-			return;
-		}
-
-		$term = get_term($term_id);
-
-		if(!$term ||
-			$term->taxonomy != self::$entity){
-			return;
-		}
-
-		$fields = apply_filters(
-			'ainsys_process_update_fields_' . self::$entity,
-			$this->prepare_data($term_id),
-			$term_id
-		);
-
-		$this->send_data( $term_id, self::$entity, self::$action, $fields );
-	}
 
 	/**
-	 * @param $term
-	 * @param $tt_id
-	 * @param $deleted_term
-	 * @param $object_ids
-	 *
-	 * Sends delete post details to AINSYS
-	 */
-	public function process_delete( $term, $tt_id, $deleted_term, $object_ids ): void {
-
-		self::$action = 'DELETE';
-
-		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
-			return;
-		}
-
-		$fields = apply_filters(
-			'ainsys_process_delete_fields_' . self::$entity,
-			$this->prepare_data( $term ),
-			$term
-		);
-
-		$this->send_data( $term, self::$entity, self::$action, $fields );
-
-	}
-
-	/**
-	 * @param $term_id
-	 * @param $tt_id
-	 * @param $args
-	 *
 	 * Sends new Product Category details to AINSYS
+	 *
+	 * @param $term_id
 	 */
-	public function process_create( $term_id, $tt_id, $args ): void {
+	public function process_create( $term_id ): void {
 
 		self::$action = 'CREATE';
 
@@ -98,7 +40,7 @@ class Process_Product_Cat extends Process implements Hooked{
 
 		$fields = apply_filters(
 			'ainsys_process_create_fields_' . self::$entity,
-			$this->prepare_data($term_id),
+			$this->prepare_data( $term_id ),
 			$term_id
 		);
 
@@ -106,16 +48,61 @@ class Process_Product_Cat extends Process implements Hooked{
 
 	}
 
+
+	/**
+	 * Sends updated Product Cat details to AINSYS.
+	 *
+	 * @param $term_id
+	 */
+	public function process_update( $term_id ): void {
+
+		self::$action = 'UPDATE';
+
+		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
+			return;
+		}
+
+		$fields = apply_filters(
+			'ainsys_process_update_fields_' . self::$entity,
+			$this->prepare_data( $term_id ),
+			$term_id
+		);
+
+		$this->send_data( $term_id, self::$entity, self::$action, $fields );
+	}
+
+
+	/**
+	 * @param $term_id
+	 */
+	public function process_delete( $term_id ): void {
+
+		self::$action = 'DELETE';
+
+		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
+			return;
+		}
+
+		$fields = apply_filters(
+			'ainsys_process_delete_fields_' . self::$entity,
+			$this->prepare_data( $term_id ),
+			$term_id
+		);
+
+		$this->send_data( $term_id, self::$entity, self::$action, $fields );
+
+	}
+
+
 	/**
 	 * Sends updated Product Category details to AINSYS.
 	 *
-	 * @param       $product_cat_id
+	 * @param       $term_id
 	 * @param       $product_cat
-	 * @param       $update
 	 *
 	 * @return array
 	 */
-	public function process_checking( $product_cat_id, $product_cat, $update ): array {
+	public function process_checking( $term_id, $product_cat ): array {
 
 		self::$action = 'CHECKING';
 
@@ -129,33 +116,24 @@ class Process_Product_Cat extends Process implements Hooked{
 
 		$fields = apply_filters(
 			'ainsys_process_update_fields_' . self::$entity,
-			$this->prepare_data($product_cat),
-			$product_cat_id
+			$this->prepare_data( $term_id ),
+			$term_id
 		);
 
-		return $this->send_data( $product_cat_id, self::$entity, self::$action, $fields );
+		return $this->send_data( $term_id, self::$entity, self::$action, $fields );
 	}
 
+
 	/**
-	 * @param $order_id
-	 *
-	 * @return array|mixed|void
 	 * Prepare Product Category data, for send to AINSYS
+	 *
+	 * @param $term_id
+	 *
+	 * @return array
 	 */
-	public function prepare_data( $term_id ){
+	protected function prepare_data( $term_id ): array {
 
-		$data = [];
-
-		$product_cat = get_term_by('id', $term_id, self::$entity);
-
-		if(!$product_cat){
-			return $data;
-		}
-
-		$prepare = new Prepare_Product_Cat_Data($product_cat);
-		$data = $prepare->prepare_data();
-
-		return $data;
+		return $this->get_prepare_data_tax( $term_id, self::$entity );
 
 	}
 
--- includes/wp/class-process-product-tag.php
+++ includes/wp/class-process-product-tag.php
@@ -4,12 +4,12 @@ namespace Ainsys\Connector\Woocommerce\WP;
 
 use Ainsys\Connector\Master\Conditions;
 use Ainsys\Connector\Master\Hooked;
-use Ainsys\Connector\Master\WP\Process;
-use Ainsys\Connector\Woocommerce\Prepare_Product_Tag_Data;
+use Ainsys\Connector\Master\WP\Prepare\Prepare_Taxonomies;
 
-class Process_Product_Tag extends Process implements Hooked{
+class Process_Product_Tag extends Prepare_Taxonomies implements Hooked {
 
 	protected static string $entity = 'product_tag';
+
 
 	/**
 	 * Initializes WordPress hooks for plugin/components.
@@ -17,77 +17,20 @@ class Process_Product_Tag extends Process implements Hooked{
 	 * @return void
 	 */
 	public function init_hooks() {
-		add_action('created_product_tag', [$this, 'process_create'], 10, 3);
-		add_action('delete_product_tag', [$this, 'process_delete'], 10, 4);
-		add_action('edited_product_tag', [$this, 'process_update'], 10, 3);
-	}
 
-	/**
-	 * @param $term_id
-	 * @param $tt_id
-	 * @param $args
-	 *
-	 * Sends updated post details to AINSYS.
-	 */
-	public function process_update( $term_id, $tt_id, $args ): void {
-
-		self::$action = 'UPDATE';
-
-		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
-			return;
-		}
-
-		/*if ( ! $this->is_updated( $product_id, $product, $update ) ) {
-			return;
-		}*/
-
-		if ( $args['taxonomy'] !== self::$entity ) {
-			return;
-		}
-
-		$fields = apply_filters(
-			'ainsys_process_update_fields_' . self::$entity,
-			$this->prepare_data($term_id),
-			$term_id
-		);
-
-		$this->send_data( $term_id, self::$entity, self::$action, $fields );
-	}
-
-	/**
-	 * @param $term
-	 * @param $tt_id
-	 * @param $deleted_term
-	 * @param $object_ids
-	 *
-	 * Sends delete post details to AINSYS
-	 */
-	public function process_delete( $term, $tt_id, $deleted_term, $object_ids ): void {
-
-		self::$action = 'DELETE';
-
-		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
-			return;
-		}
-
-		$fields = apply_filters(
-			'ainsys_process_delete_fields_' . self::$entity,
-			$this->prepare_data( $term ),
-			$term
-		);
-
-		$this->send_data( $term, self::$entity, self::$action, $fields );
+		add_action( 'created_' . self::$entity, [ $this, 'process_create' ], 10, 1 );
+		add_action( 'edited_' . self::$entity, [ $this, 'process_update' ], 10, 1 );
+		add_action( 'delete_' . self::$entity, [ $this, 'process_delete' ], 10, 1 );
 
 	}
 
+
 	/**
-	 * @param $term_id
-	 * @param $tt_id
-	 * @param $args
-	 *
 	 * Sends new Product Category details to AINSYS
+	 *
+	 * @param $term_id
 	 */
-	public function process_create( $term_id, $tt_id, $args ): void {
+	public function process_create( $term_id ): void {
 
 		self::$action = 'CREATE';
 
@@ -97,7 +40,7 @@ class Process_Product_Tag extends Process implements Hooked{
 
 		$fields = apply_filters(
 			'ainsys_process_create_fields_' . self::$entity,
-			$this->prepare_data($term_id),
+			$this->prepare_data( $term_id ),
 			$term_id
 		);
 
@@ -105,16 +48,63 @@ class Process_Product_Tag extends Process implements Hooked{
 
 	}
 
+
 	/**
 	 * Sends updated post details to AINSYS.
 	 *
-	 * @param       $product_tag_id
+	 * @param $term_id
+	 */
+	public function process_update( $term_id ): void {
+
+		self::$action = 'UPDATE';
+
+		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
+			return;
+		}
+
+		$fields = apply_filters(
+			'ainsys_process_update_fields_' . self::$entity,
+			$this->prepare_data( $term_id ),
+			$term_id
+		);
+
+		$this->send_data( $term_id, self::$entity, self::$action, $fields );
+	}
+
+
+	/**
+	 * Sends delete post details to AINSYS
+	 *
+	 * @param $term_id
+	 */
+	public function process_delete( $term_id ): void {
+
+		self::$action = 'DELETE';
+
+		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
+			return;
+		}
+
+		$fields = apply_filters(
+			'ainsys_process_delete_fields_' . self::$entity,
+			$this->prepare_data( $term_id ),
+			$term_id
+		);
+
+		$this->send_data( $term_id, self::$entity, self::$action, $fields );
+
+	}
+
+
+	/**
+	 * Sends updated post details to AINSYS.
+	 *
+	 * @param       $term_id
 	 * @param       $product_tag
-	 * @param       $update
 	 *
 	 * @return array
 	 */
-	public function process_checking( $product_tag_id, $product_tag, $update ): array {
+	public function process_checking( $term_id, $product_tag ): array {
 
 		self::$action = 'CHECKING';
 
@@ -128,29 +118,23 @@ class Process_Product_Tag extends Process implements Hooked{
 
 		$fields = apply_filters(
 			'ainsys_process_update_fields_' . self::$entity,
-			$this->prepare_data($product_tag_id),
-			$product_tag_id
+			$this->prepare_data( $term_id ),
+			$term_id
 		);
 
-		return $this->send_data( $product_tag_id, self::$entity, self::$action, $fields );
+		return $this->send_data( $term_id, self::$entity, self::$action, $fields );
 	}
 
+
 	/**
-	 * @param $product_tag_id
+	 * @param $term_id
 	 *
-	 * @return array|mixed|void
+	 * @return array
 	 * Prepare product data, for send to AINSYS
 	 */
-	public function prepare_data( $product_tag_id ){
+	protected function prepare_data( $term_id ): array {
 
-		$data = [];
-
-		$product_tag = get_term_by('id', $product_tag_id, 'product_tag');
-
-		$prepare = new Prepare_Product_Tag_Data($product_tag);
-		$data = $prepare->prepare_data();
-
-		return $data;
+		return $this->get_prepare_data_tax( $term_id, self::$entity );
 
 	}
 
