61775e5092a1e32a3100769de9f7d3b0bf3a0910
Артем <artikus.sol@gmail.com>
2023-03-13 17:46:30 +0400

    VER - AINSYS connector WooCommerce 4.4.0 - changelog

--- plugin.php
+++ plugin.php
@@ -3,26 +3,22 @@
 namespace Ainsys\Connector\Woocommerce;
 
 /**
- * @link              https://github.com/ainsys/ainsys-wp-connector-plugin
- * @since             3.0.0
- * @package           ainsys-connector-acf
- *
- * @wordpress-plugin
  * Plugin Name:       AINSYS connector Woocommerce Integration
  * Plugin URI: https://app.ainsys.com/
  * Description: AINSYS connector Woocommerce integration.
- * Version:           4.3.2
+ * Version:           4.4.0
  * Author:            AINSYS
  * Author URI:        https://app.ainsys.com/
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:     Ainsysconnector
+ * Text Domain:     ainsys-connector-woocommerce-integration
  * Domain Path:     /languages
  */
 
 defined( 'ABSPATH' ) || die();
+define( 'AINSYS_CONNECTOR_WOOCOMMERCE_TEXTDOMAIN', 'ainsys-connector-woocommerce-integration' );
 
-if ( version_compare( PHP_VERSION, '7.2.0' ) < 0 ) {
+if ( version_compare( PHP_VERSION, '7.4.0' ) < 0 ) {
 
 	if ( ! function_exists( 'deactivate_plugins' ) ) {
 		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
@@ -33,7 +29,7 @@ if ( version_compare( PHP_VERSION, '7.2.0' ) < 0 ) {
 		'admin_notices',
 		function () {
 			$class    = 'notice notice-error is-dismissible';
-			$message1 = __( 'Upgrade your PHP version. Minimum version - 7.2+. Your PHP version ' );
+			$message1 = __( 'Upgrade your PHP version. Minimum version - 7.4+. Your PHP version ' );
 			$message2 = __( '! If you don\'t know how to upgrade PHP version, just ask in your hosting provider! If you can\'t upgrade - delete this plugin!' );
 
 			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message1 . PHP_VERSION . $message2 ) );
