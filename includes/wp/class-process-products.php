d57d42c2494017266039c1d18e05b03c2b0e9d52
art-ainsys <112867520+art-ainsys@users.noreply.github.com>
2023-03-14 19:47:03 +0400

    RFG - отладка создания, обновления, удаления товаров (#18)
    * RFG - рефакторинг обработки проверки товаров по новым данным
    
    * RFG - переименование, перенос класса дла подключения настроек
    
    * RFG - рефаакторинг передачи данных о товаре
    
    * RFG - рефакторинг, отладка передачи данных oб аттрибутах
    
    * UPD - рандомные данные для проверки товаров и атрибутов
    
    * RFG - рефакторинг, отладка передачи данных o категориях и метках
    
    * RFG - рефакторинг, удаление лишних файлов
    
    * RFG - рефакторинг, проверка данных категорий и меток
    
    * UPD - удаление лишних логов
    
    * RFG - рефакторинг, перименование класса
    
    * ADD - новая сущньсть для приема/передачи - заказы
    
    * RFG - небольшие правки
    
    * RFG - рефакторинг вспомогательного класса
    
    * UPD - дополнительные хуки для удаление товаров
    
    * ADD - подготовка данных для передачи заказов
    
    * RFG - рефакторинг DI класса
    
    * RFG - рефакторинг получения данных товара
    
    * RFG - перенос вспомогательных классов в отдельную папку
    
    * VER - AINSYS connector WooCommerce 4.4.0 - changelog
    
    * RFG - отладка создания, обновления, удаления товаров
    
    * GIT - исключение лишних папок
    
    ---------
    
    Co-authored-by: Артем <artikus.sol@gmail.com>

--- includes/wp/class-process-products.php
+++ includes/wp/class-process-products.php
@@ -7,6 +7,7 @@ use Ainsys\Connector\Master\WP\Process;
 use Ainsys\Connector\Master\Conditions;
 use Ainsys\Connector\Woocommerce\WP\Prepare\Prepare_Product;
 use Ainsys\Connector\Woocommerce\WP\Prepare\Prepare_Product_Variation;
+use WC_Product;
 
 class Process_Products extends Process implements Hooked {
 
@@ -20,13 +21,11 @@ class Process_Products extends Process implements Hooked {
 	 */
 	public function init_hooks() {
 
-		add_action( 'woocommerce_new_product', [ $this, 'process_create' ], 10, 1 );
-		add_action( 'woocommerce_update_product', [ $this, 'process_update' ], 10, 2 );
+		add_action( 'wp_after_insert_post', [ $this, 'process_create' ], 1000, 4 );
+		add_action( 'woocommerce_update_product', [ $this, 'process_update' ], 1010, 2 );
 
-		add_action( 'woocommerce_delete_product', [ $this, 'process_delete' ], 10, 1 );
-		add_action( 'woocommerce_delete_product_variation', [ $this, 'process_delete' ], 10, 1 );
-		add_action( 'woocommerce_trash_product', [ $this, 'process_delete' ], 10, 1 );
-		add_action( 'woocommerce_trash_product_variation', [ $this, 'process_delete' ], 10, 1 );
+		add_action( 'deleted_post', [ $this, 'process_delete' ], 10, 2 );
+		add_action( 'trashed_post', [ $this, 'process_trash' ], 10, 1 );
 
 	}
 
@@ -34,15 +33,30 @@ class Process_Products extends Process implements Hooked {
 	/**
 	 * Sends new product details to AINSYS
 	 *
-	 * @param  int $product_id
+	 * @param  int  $product_id
+	 * @param       $post
+	 * @param  bool $update
+	 * @param       $post_before
 	 *
 	 * @return void
 	 */
-	public function process_create( int $product_id ): void {
+	public function process_create( int $product_id, $post, bool $update, $post_before ): void {
 
 		self::$action = 'CREATE';
 
 		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
+			return;
+		}
+
+		if ( 'publish' !== $post->post_status || is_int( wp_is_post_revision( $post ) ) || is_int( wp_is_post_autosave( $post ) ) ) {
+			return;
+		}
+
+		if ( $post_before && 'publish' === $post_before->post_status ) {
+			return;
+		}
+
+		if ( $post->post_type !== self::$entity ) {
 			return;
 		}
 
@@ -60,10 +74,23 @@ class Process_Products extends Process implements Hooked {
 	/**
 	 * Sends updated product details to AINSYS.
 	 *
-	 * @param       $product_id
-	 * @param       $product
+	 * @param  int        $product_id
+	 * @param  WC_Product $product
 	 */
-	public function process_update( $product_id, $product ): void {
+	public function process_update( int $product_id, WC_Product $product ): void {
+
+
+		if ( $_REQUEST['action'] === 'editpost' && did_action( 'woocommerce_update_product' ) > 1 ) {
+			return;
+		}
+
+		if ( ! in_array( get_post_type( $product_id ), [ self::$entity, 'product_variation' ], true ) ) {
+			return;
+		}
+
+		if ( strtotime( $product->get_date_created()->date( 'Y-m-d H:i:s' ) ) === strtotime( $product->get_date_modified()->date( 'Y-m-d H:i:s' ) ) ) {
+			return;
+		}
 
 		self::$action = 'UPDATE';
 
@@ -71,16 +98,40 @@ class Process_Products extends Process implements Hooked {
 			return;
 		}
 
-		if ( ! $this->is_updated( $product_id, $product, true ) ) {
+		$fields = apply_filters(
+			'ainsys_process_update_fields_' . self::$entity,
+			$this->prepare_data( $product_id ),
+			$product_id
+		);
+
+		$this->send_data( $product_id, self::$entity, self::$action, $fields );
+
+		clean_post_cache( $product_id );
+	}
+
+
+	/**
+	 * Sends delete post details to AINSYS
+	 *
+	 * @param  int $product_id
+	 * @param      $product
+	 *
+	 * @return void
+	 */
+	public function process_delete( int $product_id, $product ): void {
+
+		self::$action = 'DELETE';
+
+		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
 			return;
 		}
 
-		if ( get_post_type( $product_id ) !== self::$entity ) {
+if ( ! in_array( get_post_type( $product_id ), [ self::$entity, 'product_variation' ], true ) ) {
 			return;
 		}
 
 		$fields = apply_filters(
-			'ainsys_process_update_fields_' . self::$entity,
+			'ainsys_process_delete_fields_' . self::$entity,
 			$this->prepare_data( $product_id ),
 			$product_id
 		);
@@ -96,11 +147,15 @@ class Process_Products extends Process implements Hooked {
 	 *
 	 * @return void
 	 */
-	public function process_delete( int $product_id ): void {
+	public function process_trash( int $product_id ): void {
 
 		self::$action = 'DELETE';
 
 		if ( Conditions::has_entity_disable( self::$entity, self::$action ) ) {
+			return;
+		}
+
+		if ( ! in_array( get_post_type( $product_id ), [ self::$entity, 'product_variation' ], true ) ) {
 			return;
 		}
 
